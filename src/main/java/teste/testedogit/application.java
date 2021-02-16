cf584eb0506429a952fbdb8aa6b56048078ba977
Breno-SO <Breno.soliv@gmail.com>
2021-02-16 15:58:01 -0300

    Projeto enviado

--- src/main/java/teste/testedogit/application.java
+++ src/main/java/teste/testedogit/application.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package teste.testedogit;
 
 /**
@@ -10,5 +6,9 @@ package teste.testedogit;
  * @author Breno
  */
 public class application {
+    public static void main(String[] args) {
+        
+        System.out.println("Olá mundo");
+    }
     
 }
